8b92cbe8a0bebf6f2c3d5e1266ff88cca9f004f7
Ed-2000 <67806358+Ed-2000@users.noreply.github.com>
2024-04-27 07:22:30 +0300

    made edits

--- Assets/Scripts/Explosive.cs
+++ Assets/Scripts/Explosive.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Cube))]
 public class Explosive : MonoBehaviour
 {
     [SerializeField] private float _explosionRadius = 25;
@@ -11,17 +12,47 @@ public class Explosive : MonoBehaviour
     private void OnEnable()
     {
         cube = transform.GetComponent<Cube>();
-        cube.WillDisappear += Explode;
+        cube.WillDisappear += DisappearanceHandler;
     }
 
     private void OnDisable()
     {
-        cube.WillDisappear -= Explode;
+        cube.WillDisappear -= DisappearanceHandler;
+    }
+
+    private void DisappearanceHandler(List<Rigidbody> explodableObjects)
+    {
+        if (explodableObjects.Count == 0)
+            Explode();
+        else if (explodableObjects.Count > 0)
+            Explode(explodableObjects);
     }
 
     private void Explode(List<Rigidbody> explodableObjects)
     {
         foreach (var explodableObject in explodableObjects)
             explodableObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+    }
+
+    private void Explode()
+    {
+        foreach (var explodableObject in GetExplodableObjects())
+        {
+            float force = _explosionForce / transform.localScale.magnitude;
+            explodableObject.AddExplosionForce(force, transform.position, _explosionRadius);
+        }
+    }
+
+    private List<Rigidbody> GetExplodableObjects()
+    {
+        List<Rigidbody> explodableObjects = new List<Rigidbody>();
+
+        var hits = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        foreach (var hit in hits)
+            if (hit.TryGetComponent<Rigidbody>(out Rigidbody rigidbody))
+                explodableObjects.Add(rigidbody);
+
+        return explodableObjects;
     }
 }
